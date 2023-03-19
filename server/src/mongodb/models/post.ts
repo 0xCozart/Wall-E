9a828af283f516ff39a7e2804f6a4138721f68ff
alan <alanvega002@gmail.com>
2023-03-18 17:22:17 -0700

    removed compile command

--- server/src/mongodb/models/post.ts
+++ server/src/mongodb/models/post.ts
@@ -1,4 +1,4 @@
-import mongoose, {Document, Schema} from 'mongoose';
+import {Document, Schema, model} from 'mongoose';
 
 export interface IPost extends Document {
   name: string;
@@ -6,12 +6,12 @@ export interface IPost extends Document {
   photo: string;
 }
 
-const Post: Schema = new mongoose.Schema({
+const Post: Schema = new Schema<IPost>({
   name: {type: String, required: true},
   prompt: {type: String, required: true},
   photo: {type: String, required: true},
 });
 
-const PostSchema = mongoose.model<IPost>('Post', Post);
+const PostSchema = model<IPost>('Post', Post);
 
 export default PostSchema;
